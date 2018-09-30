d45cd72dc6c846d718e14a95f3affc08d8c052d7
vohungvi <vohungvi@vohungvi.com>
2018-09-30 19:13:49 +0700

    - merge branch auto capture camera

--- ObjectDetector/WinMain.cpp
+++ ObjectDetector/WinMain.cpp
@@ -10,7 +10,10 @@
 #include "TGMTdebugger.h"
 #include "TGMTfile.h"
 
-bool saveUndetected;
+bool saveUndetected = false;
+int interval;
+clock_t _time = clock();
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void OnKeyEvent(int key)
@@ -18,6 +21,10 @@ void OnKeyEvent(int key)
 	if (key == KEY_ESC)
 	{
 		GetTGMTcamera()->StopCamera();
+	}
+	else if (key == KEY_ENTER)
+	{
+		saveUndetected = !saveUndetected;
 	}
 }
 
@@ -28,8 +35,9 @@ void OnCameraFrames(std::vector<cv::Mat> frames)
 	for (int i = 0; i < frames.size(); i++)
 	{
 		std::vector<cv::Rect> rects = GetTGMTobjDetect()->Detect(frames[i]);
-		if (saveUndetected && rects.size() == 0)
+		if (saveUndetected && rects.size() == 0 && clock() - _time > interval)
 		{
+			_time = clock();
 			WriteImage(frames[i], ("img\\" + GetCurrentDateTime(true) + ".png").c_str());
 		}
 		TGMTdraw::DrawRectangles(frames[i], rects, 1, BLUE);
@@ -53,6 +61,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	GetTGMTConfig()->SetWriteDefaultValueIfNotExist(true);
 	GetTGMTConfig()->LoadSettingFromFile();
 	saveUndetected = GetTGMTConfig()->ReadValueBool("CascadeDetector", "save_undetected");
+	interval = GetTGMTConfig()->ReadValueInt("CascadeDetector", "interval", 1000);
 	if (saveUndetected)
 	{
 		TGMTfile::CreateDir("img");
